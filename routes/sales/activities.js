b8945d275753b6a0e22fdb06fa6c46365e27222a
Fatmamaged1 <fatma.m.elessawy@gmail.com>
2025-09-15 13:29:50 +0300

    add leads system

--- routes/sales/activities.js
+++ routes/sales/activities.js
@@ -1,14 +1,14 @@
 const express3 = require('express');
-const routerActivities = express3.Router();
+const router = express3.Router();
 const activityController = require('../../controllers/sales/ActivityController');
 
 
-routerActivities.post('/', activityController.createActivity);
-routerActivities.get('/', activityController.listActivities);
-routerActivities.get('/:id', activityController.getActivity);
-routerActivities.put('/:id', activityController.updateActivity);
-routerActivities.patch('/:id/complete', activityController.completeActivity);
-routerActivities.delete('/:id', activityController.deleteActivity);
+router.post('/', activityController.createActivity);
+router.get('/', activityController.listActivities);
+router.get('/:id', activityController.getActivity);
+router.put('/:id', activityController.updateActivity);
+router.patch('/:id/complete', activityController.completeActivity);
+router.delete('/:id', activityController.deleteActivity);
 
 
-module.exports = routerActivities;
+module.exports = router;
