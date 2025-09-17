20192d8357efb81c78c4cd0faebd04f3733ead81
Fatmamaged1 <fatma.m.elessawy@gmail.com>
2025-09-17 11:27:27 +0300

    add seles

--- controllers/sales/leads.js
+++ controllers/sales/leads.js
@@ -13,7 +13,7 @@ exports.createLead = async (req,res) => {
   // جلب كل الـ Leads (غير المحذوفة)
   exports.listLeads = async (req, res) => {
     try {
-      const leads = await Lead.finda({}); // Array
+      const leads = await Lead.find({}); // Array
       return res.json({ success: true, data: leads });
     } catch (err) {
       return res.status(500).json({ success: false, message: err.message });
