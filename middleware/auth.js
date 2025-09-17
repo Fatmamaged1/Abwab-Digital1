e06a72359a67071c80fa5e5aea0bf72352851ec8
Fatmamaged1 <fatma.m.elessawy@gmail.com>
2025-09-17 12:19:16 +0300

    add seles

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
-// middleware/auth.js
 const jwt = require("jsonwebtoken");
 
 exports.protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // "Bearer token"
-  if (!token) return res.status(401).json({ success: false, message: "Unauthorized" });
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = { id: decoded.id }; // أو أي بيانات أخرى مخزنة في التوكن
+    req.user = { id: decoded.userId }; // <-- نستخدم userId مش id
     next();
   } catch (err) {
-    console.error(err);
     res.status(401).json({ success: false, message: "Invalid token" });
   }
 };
